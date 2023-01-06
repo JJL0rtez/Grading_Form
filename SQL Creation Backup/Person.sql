1290e00e4327a731ee4c519b7c732b9f9a2d4385
JJL0rtez <jjlortez.jl@gmail.com>
2023-01-06 10:51:32 -0500

    Updating Persons table and adding test data.

--- SQL Creation Backup/Person.sql
+++ SQL Creation Backup/Person.sql
@@ -1,7 +1,29 @@
-CREATE TABLE Person(
+CREATE TABLE Persons(
     personID INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(100),
     lastName VARCHAR(100),
-    BeltLevel VARCHAR(100),
-    EMAIL VARCHAR(100)
+    beltLevel VARCHAR(100),
+    email VARCHAR(100),
+	dateOfBirth DATE
 );
+
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Sam','North','Red','snorth@gmail.com','1962-05-20');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Kimberly','North','Blue Stripe','snorth@gmail.com','2012-11-10');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Kylie','North','White','snorth@gmail.com','2016-04-15');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Jasmine','Newman','Brown Stripe','catgirl457@gmail.com','1972-02-12');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Kevin','Scott','Green','kscott@gmail.com','1998-04-14');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Mary','Rutherford','Yellow','mrutherford@gmail.com','2001-05-20');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Tom','Simpson','Black','sblack@gmail.com','2000-01-11');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Rachel','Stewart','Second Black Stripe','karatelady1231@gmail.com','2013-12-21');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Stephanie','Wright','Blue','oliverwright@gmail.com','2015-09-19');
+INSERT INTO Persons (firstName,lastName,beltLevel,email,dateOfBirth) VALUES
+('Adam','Wright','Yellow','oliverwright@gmail.com','2009-11-26');
