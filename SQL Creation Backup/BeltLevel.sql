258d81f8975b78025f719920ae387a4e5aa7e655
JJL0rtez <jjlortez.jl@gmail.com>
2023-01-08 04:31:46 -0500

    Added belt level table

---
+++ SQL Creation Backup/BeltLevel.sql
@@ -0,0 +1,24 @@
+CREATE TABLE BeltLevel(
+    beltLevelID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    BeltRank INT,
+);
+
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('White',1);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Yellow Stripe',2);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Yellow',3);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Orange Stripe',4);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Orange',5);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Red Stripe',6);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Red',7);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Green Stripe',8);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Green',9);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Blue Stripe',10);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Blue',11);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Brown Stripe',12);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Brown',13);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('First Black Stripe',14);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Second Black Stripe',15);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('First Degree Black Belt',16);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Second Degree Black Belt',17);
+INSERT INTO BeltLevel (Name,BeltRank) VALUES ('Third Degree Black Belt',18);
